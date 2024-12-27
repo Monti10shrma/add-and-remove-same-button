b831370d3ac91883e143b1a2f95896f8674fc6b3
Manvendra Sharma <115574050+Monti10shrma@users.noreply.github.com>
2024-12-28 03:38:10 +0530

    Add and remove same button
    To create a button that toggles between two states: "Add" and "Remove." Clicking the button will update the text and action of the button dynamically, and reflect changes in a corresponding status message.

---
+++ script.js
@@ -0,0 +1,21 @@
+var istatus = document.querySelector("h5");
+        var addfriend = document.querySelector("#add");
+        var check=0
+
+        addfriend.addEventListener("click", function () {
+            if (check==0){
+                istatus.innerHTML = "Friends";
+            istatus.style.color = "green";
+            addfriend.innerHTML="Remove friend"
+            check=1; 
+            }
+            else{
+                istatus.innerHTML = "Stranger";
+            istatus.style.color = "red";
+            addfriend.innerHTML="Add friend" 
+            check=0;
+             }
+            
+       } )
+       
+       
